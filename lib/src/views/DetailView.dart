65bf7e73d6027aaff2c93232a039146ead2ccbcb
Victor <victor.morales@etu.univ-grenoble-alpes.fr>
2025-01-07 09:56:54 +0100

    Base page redirect

---
+++ lib/src/views/DetailView.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( // Root widget
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Home Page'),
+        ),
+        body: Center(
+          child: Builder(
+            builder: (context) {
+              return Column(
+                children: [
+                  const Text('Hello, World!'),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('Click!');
+                    },
+                    child: const Text('A button'),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
